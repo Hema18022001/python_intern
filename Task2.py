0e7530684ca9c6c94a912377d26deac8344cf2f5
Hema18022001 <67794579+Hema18022001@users.noreply.github.com>
2020-11-26 21:20:49 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,13 @@
+a = b = c = 20
+print("Divide a by 10:", a / 10)
+print("Multiply b by 30:", b * 30)
+print("Add c:", c + 50)
+
+x = "happy"
+y = x.replace("p", "G", 1)
+print(y)
+
+a = 18
+b = 1.8
+print(float(a))
+print(int(b))
