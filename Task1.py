7b9c0ebef99dd106ab4d1606d4c3881a2b882b80
Hema18022001 <67794579+Hema18022001@users.noreply.github.com>
2020-11-26 21:13:58 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,25 @@
+print("30 days 30 hours challenge")
+
+Hours="thirty"
+print(Hours)
+
+Days="Thirty days"
+print(Days[0])
+print(Days[3])
+
+Challenge="I will win"
+print(Challenge[7:10])
+print(len(Challenge))
+print(Challenge.lower())
+
+a="30 days"
+b="30 days"
+c=a +""+ b
+print(c)
+
+text="Thirty days and Thirty hours"
+x=text.casefold()
+print(x)
+
+y="Hema"
+print(y[1:])
