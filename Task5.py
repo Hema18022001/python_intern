ac5f782cfa193278ccea6e9a9b3617a8efbc2d59
Hema18022001 <67794579+Hema18022001@users.noreply.github.com>
2020-11-27 19:35:41 +0530

    Add files via upload

---
+++ Task5.py
@@ -0,0 +1,18 @@
+def myfunc():
+    a=int(input("a="))
+    b=int(input("b="))
+    print(f"Addition of two numbers is {a + b}")
+    print(f"Subtraction of two numbers is {a - b}")
+    print(f"Division of two numbers is {a / b}")
+    print(f"Multiplication of two numbers is {a * b}")
+myfunc()
+def covid(name,temp=98):
+    print(f"Name={name} \n Temperature={temp}")
+covid("Hema",95)
+covid("Jammy")
+
+
+
+
+
+
